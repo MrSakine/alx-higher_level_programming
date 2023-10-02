fe1fb03991d0c60a4f8924aa94d8644044d13f10
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-02 17:44:36 +0000

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,18 +10,21 @@ int check_cycle(listint_t *list)
 	listint_t *head;
 	listint_t *next;
 
-	if (list == NULL)
+	if (list == NULL || list->next == NULL)
 		return (0);
 
 	head = list;
 	next = list->next;
 
-	while (next != NULL)
+	while (next && next->next != NULL)
 	{
-		if (next == head)
+		if (head == next)
+		{
 			return (1);
+		}
 
-		next = next->next;
+		head = head->next;
+		next = next->next->next;
 	}
 
 	return (0);
