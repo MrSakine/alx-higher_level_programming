a560bddd6a7b1ec8c67789dd5043172b62e44554
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-02 11:33:18 +0000

    Update 5-print_string.py

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-for i in range(3):
-    print(str, end="")
-print(f"\n{str[:9]}", end="\n")
+print(str, str, str, end="");
+print(f"\n{str[:9]:s}", end="\n")
