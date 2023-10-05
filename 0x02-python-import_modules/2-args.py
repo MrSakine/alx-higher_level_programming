9536156e53176777a6d104ddeb775cde65885f3a
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-05 09:29:52 +0000

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 import sys
-argv = sys.argv
-if (len(argv) <= 1):
-    print("0 arguments.")
-else:
-    argc = (len(argv) - 1)
-    plural = "argument" if argc == 1 else "arguments"
-    print("{0} {1}:".format(argc, plural))
 
-    for i in range(1, argc + 1):
-        print("{0}: {1}".format((i), argv[i]))
+
+def command_line():
+    """My command line function
+
+        Args: None
+        Returns: None
+        """
+    argv = sys.argv
+    if (len(argv) <= 1):
+        print("0 arguments.")
+    else:
+        argc = (len(argv) - 1)
+        plural = "argument" if argc == 1 else "arguments"
+        print("{0} {1}:".format(argc, plural))
+
+        for i in range(1, argc + 1):
+            print("{0}: {1}".format((i), argv[i]))
+
+
+if (__name__ == "__main__"):
+    command_line()
