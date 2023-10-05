be9d14c72d5be010348985c1b48aa9a7c1127b15
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-05 09:45:38 +0000

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,8 @@ import sys
 
 if (__name__ == "__main__"):
     sumOfNumber = 0
-    
+
     for i in range(len(sys.argv) - 1):
         sumOfNumber += int(sys.argv[i + 1])
-        
+
     print(sumOfNumber)
