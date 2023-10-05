1a8e1031cf8bb60cd132ca66a27f6f67913c5471
MrSakine <msakine20@gmail.com>
2023-10-05 02:35:26 -0700

    prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+
+
+def command_line_addition(input: list[str] = []):
+    """My command line addition function
+
+        Args:
+            input: list of strings
+        Returns: None
+    """
+    argc = (len(input) - 1)
+
+    if (len(input) <= 1):
+        print("0")
+    else:
+        sumOfNumber: int = 0
+        for i in range(1, argc + 1):
+            try:
+                temp = int(str(input[i]))
+                sumOfNumber += temp
+            except Exception:
+                pass
+
+        print(sumOfNumber)
+
+
+if (__name__ == "__main__"):
+    command_line_addition(input=sys.argv)
