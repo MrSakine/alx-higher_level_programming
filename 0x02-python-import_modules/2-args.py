8e161588bc51b003e04e359b266ef8f143f2d25e
MrSakine <msakine20@gmail.com>
2023-10-05 02:27:34 -0700

    prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv
+if (len(argv) <= 1):
+    print("0 arguments.")
+else:
+    argc = (len(argv) - 1)
+    plural = "argument" if argc == 1 else "arguments"
+    print("{0} {1}:".format(argc, plural))
+
+    for i in range(1, argc + 1):
+        print("{0}: {1}".format((i), argv[i]))
