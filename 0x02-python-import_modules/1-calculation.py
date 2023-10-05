735b1791acfc2e69299bbcb6294d533f9e5956cb
MrSakine <msakine20@gmail.com>
2023-10-05 02:22:24 -0700

    imports functions from the file calculator_1.py, does some Maths, and prints the result

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if (__name__ == "__main__"):
+    from calculator_1 import *
+
+    a = 10
+    b = 5
+    add(a=a, b=b)
+    sub(a=a, b=b)
+    mul(a=a, b=b)
+    div(a=a, b=b)
