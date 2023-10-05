5bffb8e6e58b20558b27c0d84031fed61533806f
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-05 09:11:33 +0000

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def add(a=1, b=2):
+a = 1
+b = 2
+def add(a, b):
     """My addition function
 
     Args:
@@ -14,4 +16,4 @@ def add(a=1, b=2):
 
 
 if (__name__ == "__main__"):
-    add()
+    add(a=a, b=b)
