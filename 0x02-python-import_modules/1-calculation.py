409f390a8104c2f3470b17bf08072709aa360a02
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-05 09:26:01 +0000

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if (__name__ == "__main__"):
 
     a = 10
     b = 5
-    add(a=a, b=b)
-    sub(a=a, b=b)
-    mul(a=a, b=b)
-    div(a=a, b=b)
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+    print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+    print("{0} / {1} = {2}".format(a, b, div(a, b)))
