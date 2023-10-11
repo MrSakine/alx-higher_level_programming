87f9acc8bdfdf42d576a2e5ae627b10e354d141c
MrSakine <msakine20@gmail.com>
2023-10-11 12:16:12 -0700

    a function that converts a Roman numeral to an integer.

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romains = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    res = 0
+    currentRomain = None
+    nextRomain = None
+    n = len(roman_string)
+    i, a, b = 0, 0, 0
+    while (i < n):
+        currentRomain = list(filter(lambda x: x == roman_string[i], romains))
+        if (i + 1 < n):
+            nextRomain = list(
+                filter(lambda y: y == roman_string[i + 1], romains))
+
+            a = romains.get(currentRomain[0])
+            b = romains.get(nextRomain[0])
+
+            if (a >= b):
+                res += a
+                i += 1
+            else:
+                res += (b - a)
+                i += 2
+        else:
+            res += a
+            i += 1
+
+    return (res)
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -35,3 +35,6 @@ a function that returns a key with the biggest integer value.
 
 # 11-multiply_list_map.py
 a function that returns a list with all values multiplied by a number without using any loops.
+
+# 12-roman_to_int.py
+a function that converts a Roman numeral to an integer.
