59c92d5af463cffbc83e855e8d7ea81391a9f4a7
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-11 12:40:18 +0000

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,9 +3,11 @@ def best_score(a_dictionary):
     if (a_dictionary is None):
         return (None)
     items = [[i, j] for (i, j) in a_dictionary.items()]
-    bestScore = items[0][1] if len(items) > 0 else None
+    bestScore = items[0][1] if len(items) > 0 else 0
+    name = None
     for i in items:
         n = i[1]
         if (n > bestScore):
             bestScore = n
-    return (bestScore)
+            name = i[0]
+    return (name)
