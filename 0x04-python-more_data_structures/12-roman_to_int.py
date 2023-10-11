bb032704594716c62a91cdb9490cb633a9765dbb
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-11 19:22:30 +0000

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if (not roman_string or roman_string is None):
+        return (0)
     romains = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     res = 0
     currentRomain = None
@@ -8,6 +10,9 @@ def roman_to_int(roman_string):
     i, a, b = 0, 0, 0
     while (i < n):
         currentRomain = list(filter(lambda x: x == roman_string[i], romains))
+        if (len(currentRomain) == 0):
+            res = 0
+            break
         a = romains.get(currentRomain[0])
         if (i + 1 < n):
             nextRomain = list(
