04ae48588f5b3ca088722fbf2fded0e486a70c8d
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-10-23 10:22:30 +0000

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     printed = True
     try:
         i = int(value)
-        print("{}".format(i))
+        print("{:d}".format(i))
     except Exception:
         printed = False
     return (printed)
