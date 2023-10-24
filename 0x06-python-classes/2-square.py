6467acec7da3868f53875b780d42fff2d6ae264c
MrSakine <msakine20@gmail.com>
2023-10-24 02:22:18 -0700

    a class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module demonstrates how to declare a class Square
+with an attribute named size and some validations about it
+"""
+
+
+class Square:
+    """
+    A square class definition
+
+    Attributes:
+        size (:obj:`int`, optional): The size of the square
+    """
+
+    def __init__(self, size=0) -> None:
+        """
+        Size attribute initialization. The default value is zero,
+        and it will throw error if the given value is
+        not an integer or less than zero
+
+        Args:
+            size (:obj:`int`, optional): The size of the square,
+            default value is zero
+        """
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -5,3 +5,6 @@ an empty class Square that defines a square
 
 # 1-square.py
 a class Square that defines a square by: (based on 0-square.py)
+
+# 2-square.py
+a class Square that defines a square by: (based on 1-square.py)
