ee410e9ff4db33adda2a732970e3cf46338e3d80
MrSakine <msakine20@gmail.com>
2023-11-06 03:26:03 -0800

    a function that returns True if the object is exactly an instance of the specified class ; otherwise False

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+<2-is_same_class.py> module
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance
+    of the specified class ; otherwise False
+
+    Attributes:
+        obj (any): the object
+        a_class (any): the class to check
+
+    Returns: a bool
+    """
+    return (isinstance(obj, a_class) and a_class is not object)
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -5,3 +5,6 @@ a function that returns the list of available attributes and methods of an objec
 
 ## 1-my_list.py, tests/1-my_list.txt
 a class MyList that inherits from list
+
+## 2-is_same_class.py
+a function that returns True if the object is exactly an instance of the specified class ; otherwise False
