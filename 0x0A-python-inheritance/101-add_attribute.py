d5fd27523acc79a497c83e2bad8c49184cb3bc17
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-11-06 12:56:05 +0000

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -15,7 +15,7 @@ def add_attribute(a_class, name, value):
     name (any): attribute name
     value (any): attribute value
     """
-    if (type(a_class) in [int, str, float, list, set, tuple, None]):
+    if (not hasattr(a_class, "__dict__")):
         raise TypeError("can't add new attribute")
 
     setattr(a_class, name, value)
