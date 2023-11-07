17cb8944878c39f824d5f3ebfeff25141fbcadc6
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-11-07 07:58:24 +0000

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,9 @@ A function that reads a text file (UTF8) and prints it to stdout
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout
+
+    Attributes:
+    filename (:obj:`str`, optional): name of the file
     """
     with open(file=filename, encoding="utf-8") as file:
         print(file.read())
