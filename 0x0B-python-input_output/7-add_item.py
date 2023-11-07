9a9e17b4a23dd3b4a18ba304bc89501011731b46
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-11-07 19:10:19 +0000

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,23 +4,10 @@ A script that adds all arguments to a Python list, and then save them to a file
 """
 import sys
 
-"""
-If no argument provided, fill the file with an empty list
-Otherwise, add the provided arguments to the list in the file
-"""
 if __name__ == "__main__":
-    """
-    Save json file import
-    """
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    """
-    Load json file import
-    """
     load_from_json_file = __import__(
         '6-load_from_json_file').load_from_json_file
-    """
-    Save json data filename
-    """
     filename = "add_item.json"
     try:
         items = load_from_json_file(filename)
