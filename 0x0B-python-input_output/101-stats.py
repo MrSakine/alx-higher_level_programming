64880e0de7aac4261ea00783973a201b4fb40d59
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-11-08 07:51:52 +0000

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -12,25 +12,25 @@ if __name__ == "__main__":
     counter = 1
     try:
         for line in sys.stdin:
-            lines.append(line)
-        for line in lines:
             inputs = line.split()
-            file_size = inputs[-1]
-            status_code = inputs[-2]
-            file_sizes.append(int(file_size))
-            status_codes.append(int(status_code))
+            if (len(inputs) >= 9):
+                file_size = inputs[-1]
+                status_code = inputs[-2]
+                file_sizes.append(int(file_size))
+                status_codes.append(int(status_code))
+                counter += 1
 
-            if ((counter % 10) == 0 or len(lines) < 9):
+            if ((counter % 10) == 0):
                 print("File size: {:d}".format(sum(file_sizes)))
                 for code in sorted(default_status_codes):
                     code_total = sum([1 for i in status_codes if i == code])
                     if (code_total > 0):
                         print("{}: {:d}".format(str(code), code_total))
-
-            counter += 1
     except KeyboardInterrupt:
-        print("File size: {:d}".format(sum(file_sizes)))
-        for code in sorted(default_status_codes):
-            code_total = sum([1 for i in status_codes if i == code])
-            if (code_total > 0):
-                print("{}: {:d}".format(str(code), code_total))
+        pass
+
+print("File size: {:d}".format(sum(file_sizes)))
+for code in sorted(default_status_codes):
+    code_total = sum([1 for i in status_codes if i == code])
+    if (code_total > 0):
+        print("{}: {:d}".format(str(code), code_total))
