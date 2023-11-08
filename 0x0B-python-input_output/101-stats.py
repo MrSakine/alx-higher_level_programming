56463c0d46b75d522018a715efd15c06b51251cf
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-11-08 07:43:31 +0000

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,16 +8,19 @@ if __name__ == "__main__":
     file_sizes = []
     status_codes = []
     default_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    lines = []
     counter = 1
     try:
         for line in sys.stdin:
+            lines.append(line)
+        for line in lines:
             inputs = line.split()
             file_size = inputs[-1]
             status_code = inputs[-2]
             file_sizes.append(int(file_size))
             status_codes.append(int(status_code))
 
-            if ((counter % 10) == 0):
+            if ((counter % 10) == 0 or len(lines) < 9):
                 print("File size: {:d}".format(sum(file_sizes)))
                 for code in sorted(default_status_codes):
                     code_total = sum([1 for i in status_codes if i == code])
