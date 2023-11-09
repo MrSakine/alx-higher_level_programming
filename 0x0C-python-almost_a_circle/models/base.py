b31b2206f7aac4ae42e3f4bb267f59507c2f962f
MrSakine <115190839+MrSakine@users.noreply.github.com>
2023-11-09 11:19:54 +0000

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 """
+Base class module, this class is used by the other ones as parent
 """
 import json
 
 
 class Base():
+    """
+    Number of objects at each new instance if id is None
+    """
     __nb_objects = 0
 
     def __init__(self, id=None) -> None:
+        """
+        If id is not None, assign the public instance attribute id
+        with this argument value otherwise, increment __nb_objects
+        and assign the new value to the public instance attribute id
+
+        Attributes:
+            id (any): id of the instance
+        """
         if (id is None):
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
@@ -16,17 +28,27 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        A static method to convert a list of dictionnaries to
+        json representation
+        """
         if (list_dictionaries is None or len(list_dictionaries) <= 0):
             return ("[]")
         return (json.dumps(list_dictionaries))
 
     def from_json_string(json_string):
+        """
+        Convert a JSON representation to JSON object
+        """
         if (json_string is None):
             return ([])
         return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """
+        Save a list of objects to a file as json representation
+        """
         items = []
         name = "{}.json".format(cls.__name__)
         if (list_objs is not None):
@@ -38,6 +60,9 @@ class Base():
 
     @classmethod
     def create(cls, **dictionary):
+        """
+        Create a new instance from a dictionnary
+        """
         dummy = (1, 1)
         instance = cls(*dummy)
         cls.update(instance, **dictionary)
@@ -45,6 +70,10 @@ class Base():
 
     @classmethod
     def load_from_file(cls):
+        """
+        Load a list of instances of the class from a file
+        named by the class name (<name>.json)
+        """
         name = "{}.json".format(cls.__name__)
         data = ""
         try:
