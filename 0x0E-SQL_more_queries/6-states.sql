a6fa50dac0babf5ed20bd0aa258ca1719ea06020
MrSakine <msakine20@gmail.com>
2023-12-13 11:58:56 -0800

    creates a database, hbtn_0d_usa & a table (on created database), states on your MySQL server

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates a database, hbtn_0d_usa & a table (on created database), states on your MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+);
