3d7d61574ff435931fe83efa24d13d140a0ad30e
MrSakine <msakine20@gmail.com>
2023-12-13 12:27:54 -0800

    uses the hbtn_0d_tvshows_rate database to list all tv shows by their rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,5 @@
+-- uses the hbtn_0d_tvshows_rate database to list all tv shows by their rating
+SELECT title, SUM(rate) AS rating FROM tv_shows
+JOIN tv_show_ratings ON id = show_id
+GROUP BY show_id
+ORDER BY rating DESC;
