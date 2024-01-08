d534619c5ebb0e33e2ff7c67929e7562727eae8f
MrSakine <msakine20@gmail.com>
2024-01-08 05:56:52 -0800

    a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const notANumber = 'Missing size';
+const parsedNumber = Number.parseInt(arg);
+if (arg === undefined || isNaN(parsedNumber)) {
+  console.log(notANumber);
+} else {
+  for (let i = 0; i < parsedNumber; i++) {
+    console.log('X'.repeat(parsedNumber));
+  }
+}
