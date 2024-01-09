504f480e86fff3ab46857c63b11187eb058a5c97
MrSakine <115190839+MrSakine@users.noreply.github.com>
2024-01-09 19:53:41 +0000

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const fs = require('node:fs');
+const fs = require('fs');
 const args = process.argv.splice(2);
 const fileA = fs.readFileSync(args[0], 'utf8');
 const fileB = fs.readFileSync(args[1], 'utf8');
