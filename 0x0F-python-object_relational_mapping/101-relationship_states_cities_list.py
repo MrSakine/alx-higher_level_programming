420fb0f6c44146ecdc4eff9e215fe0be2bb7aeea
MrSakine <115190839+MrSakine@users.noreply.github.com>
2024-01-13 02:22:55 +0000

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -7,7 +7,7 @@ import sys
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 from relationship_state import State
-from relationship_city import City
+from relationship_city import Base, City
 
 if __name__ == "__main__":
     engine = create_engine(
@@ -15,6 +15,7 @@ if __name__ == "__main__":
         .format(
             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
     )
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id)
