1fc12a72aca9d2ef29dcb8010bf47243480579a2
MrSakine <115190839+MrSakine@users.noreply.github.com>
2024-01-25 13:12:55 +0000

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,26 +7,22 @@ def find_peak(list_of_integers):
     Finds a peak in a list of unsorted integers
     """
     n = len(list_of_integers)
-    return find_peak_util(list_of_integers, 0, n - 1)
+    return find_peak_util(list_of_integers, 0, n - 1, n)
 
 
-def find_peak_util(list_of_integers, start, end):
+def find_peak_util(list_of_integers, start, end, size):
     """
     Recursive method that handles the logic of find_peak method
     """
-    try:
+    if (start <= end):
         mid = start + (end - start) // 2
         current = list_of_integers[mid]
         left = list_of_integers[mid - 1]
         right = list_of_integers[mid + 1]
 
-        if (mid == 0 or left <= current and right <= current):
+        if (mid == 0 or left <= current and current >= right):
             return current
 
         if left > current:
-            return find_peak_util(list_of_integers, start, mid - 1)
-
-        if right > current:
-            return find_peak_util(list_of_integers, mid + 1, end)
-    except Exception:
-        pass
+            return find_peak_util(list_of_integers, start, mid - 1, size)
+        return find_peak_util(list_of_integers, mid + 1, end, size)
