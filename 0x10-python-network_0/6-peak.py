ceba91a9d9a20d82057140802c1a65bf06eb3946
MrSakine <115190839+MrSakine@users.noreply.github.com>
2024-01-25 17:09:28 +0000

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,24 +6,21 @@ def find_peak(list_of_integers):
     """
     Finds a peak in a list of unsorted integers
     """
-    n = len(list_of_integers)
-    return find_peak_util(list_of_integers, 0, n - 1, n)
-
-
-def find_peak_util(list_of_integers, start, end, size):
-    """
-    Recursive method that handles the logic of find_peak method
-    """
-    if (start <= end):
-        mid = start + (end - start) // 2
-        current = list_of_integers[mid]
-        left = list_of_integers[mid - 1]
-        right = list_of_integers[mid + 1]
-
-        if ((mid == 0 or left <= current) and
-                (mid == size - 1 or right <= current)):
-            return current
-        elif left > current:
-            return find_peak_util(list_of_integers, start, mid - 1, size)
+    if len(list_of_integers) > 0:
+        size = len(list_of_integers)
+        if size == 1:
+            return list_of_integers[0]
+        elif size == 2:
+            return max(list_of_integers)
         else:
-            return find_peak_util(list_of_integers, mid + 1, end, size)
+            mid = int(size / 2)
+            current = list_of_integers[mid]
+            left = list_of_integers[mid - 1]
+            right = list_of_integers[mid + 1]
+
+            if current > left and current > right:
+                return current
+            elif current < left:
+                return find_peak(list_of_integers[:mid])
+            else:
+                return find_peak(list_of_integers[mid + 1:])
